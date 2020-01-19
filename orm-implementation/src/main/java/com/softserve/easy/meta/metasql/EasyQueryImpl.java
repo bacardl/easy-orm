51d65582ed7620976c1bf4ddf097357a6484f9e6
Yuliia Korabelska <yuliia_korabelska@epam.com>
2020-01-19 20:52:23 +0200

    created regexp validation for incoming query in  easy query language

--- orm-implementation/src/main/java/com/softserve/easy/meta/metasql/EasyQueryImpl.java
+++ orm-implementation/src/main/java/com/softserve/easy/meta/metasql/EasyQueryImpl.java
@@ -1,12 +1,26 @@
 package com.softserve.easy.meta.metasql;
 
+import com.softserve.easy.exception.OrmException;
+
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class EasyQueryImpl implements EasyQuery{
-    private String string;
+    private String userQuery;
+    private StringBuilder query;
 
     public EasyQueryImpl(String query) {
+        this.userQuery = query;
+        this.query = new StringBuilder();
+    }
 
+    private void validate(){
+        if(matchesDelete()|| matchesInsert()|| matchesSelect()||matchesUpdate()){
+
+        } else {
+            throw new OrmException("query type not specified : " + userQuery);
+        }
     }
 
     @Override
@@ -33,4 +47,50 @@ public class EasyQueryImpl implements EasyQuery{
     public ObjectQuery setParameter(int position, Object value) {
         return null;
     }
+
+    private String prepareInsertQuery(){
+//        String insertQ = new InsertQuery();
+        return null;
+    }
+
+    private String prepareDeleteQuery(){
+        return null;
+    }
+
+    private String prepareUpdateQuery(){
+        return null;
+    }
+
+    private String prepareSelectQuery(){
+        return null;
+    }
+
+    public boolean matchesSelect(){
+        Pattern pattern = Pattern.compile("(select )?from \\w+( where (\\w+(\\s)*=(\\s)*(:)?\\w+(,)*(\\s)*)+)?", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(userQuery);
+        return matcher.matches();
+    }
+
+    public boolean matchesDelete(){
+        Pattern pattern = Pattern.compile("delete (from )?\\w+( where (\\w+(\\s)*=(\\s)*:\\w+(,)*(\\s)*)+)?", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(userQuery);
+        return matcher.matches();
+    }
+
+    public boolean matchesUpdate(){
+        Pattern pattern = Pattern.compile("update \\w+ set (\\w+\\s*=\\s*:\\w+,*(\\s)*)+((\\s)*(where\\s(\\w+(\\s)*=(\\s)*:(\\s)*\\w+(,)*(\\s)*)+))*", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(userQuery);
+        return matcher.matches();
+
+    }
+
+    public boolean matchesInsert(){
+        Pattern pattern = Pattern.compile("insert into \\w+(\\s)*\\((\\w+(,)*(\\s)*)*\\) select (\\w+(,)*(\\s)*)* from \\w+( where\\s(\\w+(\\s)*=(\\s)*:(\\s)*\\w+(,)*(\\s)*)+)?", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(userQuery);
+        return matcher.matches();
+
+    }
+
+
+
 }
---
+++ orm-implementation/src/test/java/com/softserve/easy/meta/EasyQueryImplTest.java
@@ -0,0 +1,118 @@
+package com.softserve.easy.meta;
+
+import com.softserve.easy.meta.metasql.EasyQueryImpl;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+class EasyQueryImplTest {
+
+    @Test
+    void checkUpdateMatcherLowerCase() {
+        EasyQueryImpl easyQuery = new EasyQueryImpl("update ContactEntity set firstName = :nameParam, lastName = :lastNameParam" +
+                ", birthDate = :birthDateParam" +
+                " where firstName = :nameCode");
+        assertThat(easyQuery.matchesUpdate(), equalTo(true));
+    }
+
+    @Test
+    void checkUpdateMatcherUpperCase() {
+        EasyQueryImpl easyQuery = new EasyQueryImpl("UPDATE ContactEntity SET firstName = :nameParam, lastName = :lastNameParam" +
+                ", birthDate = :birthDateParam" +
+                " WHERE firstName = :nameCode");
+        assertThat(easyQuery.matchesUpdate(), equalTo(true));
+    }
+
+    @Test
+    void checkUpdateMatcherWithoutUpdate() {
+        EasyQueryImpl easyQuery = new EasyQueryImpl("ContactEntity set firstName = :nameParam, lastName = :lastNameParam" +
+                ", birthDate = :birthDateParam" +
+                " where firstName = :nameCode");
+        assertThat(easyQuery.matchesUpdate(), equalTo(false));
+    }
+
+    @Test
+    void checkDeleteMatcherWithoutParameter() {
+        EasyQueryImpl easyQuery = new EasyQueryImpl("delete from Emp where id=100");
+        assertThat(easyQuery.matchesDelete(), equalTo(false));
+    }
+
+    @Test
+    void checkDeleteMatcherLowerCase() {
+        EasyQueryImpl easyQuery = new EasyQueryImpl("delete from Emp where id= :other");
+        assertThat(easyQuery.matchesDelete(), equalTo(true));
+    }
+
+    @Test
+    void checkDeleteMatcherUpperCase() {
+        EasyQueryImpl easyQuery = new EasyQueryImpl("DELETE ContactEntity WHERE firstName = :param");
+        assertThat(easyQuery.matchesDelete(), equalTo(true));
+    }
+
+    @Test
+    void checkDeleteMatcherWithoutDelete() {
+        EasyQueryImpl easyQuery = new EasyQueryImpl("ContactEntity WHERE firstName = :param");
+        assertThat(easyQuery.matchesDelete(), equalTo(false));
+    }
+
+    @Test
+    void checkSelectMatcherLowerCase(){
+        EasyQueryImpl easyQuery = new EasyQueryImpl("select from ContactEntity where firstName = :param");
+        assertThat(easyQuery.matchesSelect(), equalTo(true));
+    }
+
+    @Test
+    void checkSelectMatcherUpperCase(){
+        EasyQueryImpl easyQuery = new EasyQueryImpl("SELECT FROM ContactEntity WHERE firstName = :param");
+        assertThat(easyQuery.matchesSelect(), equalTo(true));
+    }
+
+    @Test
+    void checkSelectMatcherWithoutSelect(){
+        EasyQueryImpl easyQuery = new EasyQueryImpl("from ContactEntity where firstName = :param");
+        assertThat(easyQuery.matchesSelect(), equalTo(true));
+    }
+
+    @Test
+    void checkSelectMatcherWithoutFrom(){
+        EasyQueryImpl easyQuery = new EasyQueryImpl("ContactEntity WHERE firstName = :param");
+        assertThat(easyQuery.matchesSelect(), equalTo(false));
+    }
+
+    @Test
+    void checkDeleteDoesNotMatchSelect(){
+        EasyQueryImpl easyQuery = new EasyQueryImpl("delete from Emp where id= :other");
+        assertThat(easyQuery.matchesSelect(), equalTo(false));
+    }
+
+    @Test
+    void checkInsertMatcherLowerCase(){
+        EasyQueryImpl easyQuery = new EasyQueryImpl("INSERT INTO ContactEntity(firstName, lastName, birthDate) SELECT firstName2, lastName2, birthDate2 FROM ContactEntity2");
+        assertThat(easyQuery.matchesInsert(), equalTo(true));
+    }
+
+    @Test
+    void checkInsertMatcherUpperCase(){
+        EasyQueryImpl easyQuery = new EasyQueryImpl("INSERT INTO ContactEntity(firstName, lastName, birthDate) SELECT firstName2, lastName2, birthDate2 FROM ContactEntity2");
+        assertThat(easyQuery.matchesInsert(), equalTo(true));
+    }
+
+    @Test
+    void checkInsertMatcherWithCondition(){
+        EasyQueryImpl easyQuery = new EasyQueryImpl("insert into purged_accounts(id, code, status) select id, code, status from account where status=:statu");
+        assertThat(easyQuery.matchesInsert(), equalTo(true));
+    }
+
+    @Test
+    void checkMatchersNotMix(){
+        EasyQueryImpl easyQuery = new EasyQueryImpl("insert delete into purged_accounts(id, code, status) select id, code, status from account where status=:statu");
+        assertThat(easyQuery.matchesInsert(), equalTo(false));
+    }
+
+
+
+
+
+
+}
