9b25604b9db0d2484fd3102d105792d12d9de221
Dimi <komdi1999@gmail.com>
2020-01-21 15:09:56 +0200

    Interface "AbstractDao" was added in package "dao"

--- orm-client/src/main/java/com/softserve/easy/client/dao/AbstractDao.java
+++ orm-client/src/main/java/com/softserve/easy/client/dao/AbstractDao.java
@@ -1,10 +1,11 @@
-package com.softserve.easy.client;
+package com.softserve.easy.client.dao;
 
+import java.io.Serializable;
 import java.util.List;
 import java.util.Optional;
 
-public interface Dao<T> {
-    Optional<T> get(long id);
+public interface AbstractDao<T> {
+    Optional<T> get(Serializable id);
     List<T> getAll();
     void save(T t);
     void update(T t);
